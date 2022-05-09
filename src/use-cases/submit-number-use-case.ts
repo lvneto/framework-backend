e2c558249e7e0801dabb543d6a9cac9004560b84
lvneto <lauro.neto@irriga.net>
2022-05-09 16:49:13 -0300

    create find dividers

--- src/use-cases/submit-number-use-case.ts
+++ src/use-cases/submit-number-use-case.ts
@@ -1,23 +1,56 @@
-import { NumbersRepository }  from '../repositories/numbers-repository'
-
+import { NumberCreateData, NumbersRepository }  from '../repositories/numbers-repository'
 interface SubmitNumberUseCaseRequest {
   number: string;
 }
-
 export class SubmitNumberUseCase {
   constructor(
     private NumbersRepository: NumbersRepository,
   ) {}
 
-  async execute(request: SubmitNumberUseCaseRequest) {
-    const { number } = request;
+  async execute(request: SubmitNumberUseCaseRequest) : Promise<NumberCreateData>{
+    let { number } = request;
 
+    let result = {
+      number,
+      dividers: [],
+      primeDividers: []
+    } as any
+    
     if (!number) {
       throw new Error('Number is required');
     }
     
-    return await this.NumbersRepository.create({
+    await this.NumbersRepository.create({
       number
-    })   
+    })      
+
+    result.number = parseInt(number)
+    result.dividers = await this.findDividers(result.number)
+    result.primeDividers = await this.findPrimeDividers(result.number)
+
+    return result
+  }
+
+  async findDividers (number: number) {    
+    const dividersList = []
+
+    for (let divider = 1; divider <= number; divider++) {
+      if (number % divider === 0) {
+          dividersList.push(divider)
+      }
+    }
+    return dividersList
+  }
+
+  async findPrimeDividers (number: number) {
+    const primeDividerList = []
+
+    for (let divider = 1; divider <= number / 2; divider ++) {
+      if (number % divider) {
+        primeDividerList.push(divider)
+      }
+    }
+   
+    return primeDividerList
   }
 }
